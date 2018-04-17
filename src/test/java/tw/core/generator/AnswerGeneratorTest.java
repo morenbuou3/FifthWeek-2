b95fb4e41ca44b413a50ed0c71e60b6bc719af24
king <369558172@qq.com>
2018-04-17 22:30:52 +0800

    ADD TEST METHOD test_should_return_an_given_random_answer

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,42 @@
 package tw.core.generator;
 
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
 
+    private RandomIntGenerator randomIntGenerator;
+    private AnswerGenerator answerGenerator;
+
+    @Before
+    public void setUp() throws Exception {
+        randomIntGenerator = mock(RandomIntGenerator.class);
+        answerGenerator = new AnswerGenerator(randomIntGenerator);
+    }
+
+    @Test
+    public void test_should_return_an_given_random_answer() throws OutOfRangeAnswerException {
+        //Given
+        String expectAnswer = "1 2 3 4";
+
+        //When
+        when(randomIntGenerator.generateNums(9, 4)).thenReturn(expectAnswer);
+
+        //Then
+        assertThat(answerGenerator.generate().toString(), is(expectAnswer));
+
+    }
 }
 
