311feb8fd802c6efa7ae302daa0b3f555822e9bd
king <369558172@qq.com>
2018-04-18 10:40:41 +0800

    ADD TEST METHOD test_answer_is_1234_should_return_Record_with_12_when_check

--- src/test/java/tw/core/AnswerTest.java
+++ src/test/java/tw/core/AnswerTest.java
@@ -4,9 +4,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import tw.core.exception.OutOfRangeAnswerException;
+import tw.core.model.Record;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * 在AnswerTest文件中完成Answer中对应的单元测试
@@ -89,4 +92,20 @@ public class AnswerTest {
         assertThat(answer.getIndexOfNum(num), is(expect));
     }
 
+    @Test
+    public void test_answer_is_1234_should_return_Record_with_12_when_check() {
+        //Given
+        String expectAnswer = "1 2 3 4";
+        Answer answer = Answer.createAnswer(expectAnswer);
+        String inputAnswer = "1 3 2 5";
+        Answer inAnswer = Answer.createAnswer(inputAnswer);
+        Record record = mock(Record.class);
+        int[] expect = {1,2};
+
+        //When
+        when(record.getValue()).thenReturn(expect);
+
+        //Then
+        assertThat(answer.check(inAnswer).getValue(), is(record.getValue()));
+    }
 }
